e27c5e7291767af9c42921bde0c55212d6410c0c
Zuleto1231 <mzueta@unal.edu.co>
2023-08-21 16:43:25 -0500

    Prueba 2

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -10,7 +10,13 @@ public class Auto {
 	static int cantidadCreados;
 	
 	int cantidadAsientos(){
-		return asientos.length;
+		int cantidad=0;
+		for (int i=0;i<asientos.length;i++) {
+			if (asientos[i]!=null) {
+				cantidad++;
+			}
+		}
+		return cantidad;
 		
 		
 	}	
@@ -19,9 +25,11 @@ public class Auto {
 		boolean integridad=true;
 		if (motor.registro==this.registro) {
 			for (int i=0;i<asientos.length;i++) {
-				if (asientos[i].registro != this.registro) {
-					integridad=false;
-					break;}
+				if (asientos[i]!=null) {
+					if (asientos[i].registro != this.registro) {
+						integridad=false;
+						break;}
+				}
 			}
 		}else {integridad=false;}
 		
